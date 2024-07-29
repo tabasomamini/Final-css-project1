dedcda236f7c7b40c4f453eacff7b821fbf59cf3
tabssom <tabasomaminiii@gmail.com>
2024-07-29 10:24:07 +0430

    fisst c

---
+++ project/script.js
@@ -0,0 +1,33 @@
+const swiper = new Swiper('.slider-wrapper', {
+   
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets:true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    //responsive breakpoints
+ breakpoints :{
+
+    0:{
+        slidesperview:1
+    },
+    620:{
+        slidesperview:2
+    },
+    1024:{
+        slidesperview:3
+    }
+  }
+  
+  });
